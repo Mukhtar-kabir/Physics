6472b6676cd242f95bd2abdb442eb37f651ba6fd
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2024-01-14 00:08:11 +0100

    Update

--- dist/script.js
+++ dist/script.js
@@ -25,3 +25,18 @@ $(document).ready(function () {
     },
   });
 });
+
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function () {
+  myFunction();
+};
